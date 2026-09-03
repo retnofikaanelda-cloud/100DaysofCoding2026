03be9402ef85cb9b5fee3cd8cf36a761eac5eb2f
retnofikaanelda-cloud <retnofikaanelda@gmail.com>
2026-09-03 16:04:03 +0800

    Create Day002.java

---
+++ Day002.java
@@ -0,0 +1,15 @@
+package day002;
+public class day002 {
+    public static void main(String[] args) {
+        System.out.println("BIODATA DIRI :");
+        System.out.println("Nama      : Retno fika anelda");
+        System.out.println("NIM       : D0225032 ");
+        System.out.println("Prodi     : informatika ");
+        System.out.print("Kampus    : ");
+        System.out.print("Universitas Sulawesi Barat");
+        System.out.println();
+        System.out.printf("Angkatan  : %d%n",2025);
+        System.out.printf("Status    : %s%n","Mahasiswa");
+        System.out.printf("BB        : %.1f%n",42.5);
+    }
+}
