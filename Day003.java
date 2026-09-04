16593029e80237f3d66096532abb077b2fe0d993
retnofikaanelda-cloud <retnofikaanelda@gmail.com>
2026-09-04 19:09:51 +0800

    Create Day003.java

---
+++ Day003.java
@@ -0,0 +1,15 @@
+package Day003;
+public class Day003 {
+    public static void main(String[] args) {
+        System.out.println("\'Penggunaan Escape Sequence\'");
+        System.out.println("\"BIODATA DIRI \"");
+        System.out.println("\nNama      :\tRetno fika anelda");
+        System.out.println("NIM       : \tD0225032");
+        System.out.println("Prodi     : \tinformatika ");
+        System.out.print("Kampuss\b    : ");
+        System.out.print("\tUniversitas Sulawesi Barat");
+        System.out.println();
+        System.out.printf("Angkatan  : \t%d%n ",2025);
+        System.out.printf("\rStatus    : \t%s%n " ,"Mahasiswa");
+    }
+}
