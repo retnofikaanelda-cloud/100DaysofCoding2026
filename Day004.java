d01318fd88e7f26d724e3bf3110033dcbb20459a
Retno fika anelda <retnofikaanelda@gmail.com>
2026-09-05 17:57:25 +0800

    Create Day004.java

---
+++ Day004.java
@@ -0,0 +1,14 @@
+package Day004;
+public class Day004 {
+    public static void main (String[]args){
+        byte a=100;
+        short b=1000;
+        int c=1000000;
+        long d=1000000000;
+        System.out.println("Seratus     =" +a);
+        System.out.println("Seribu      =" +b);
+        System.out.println("Satu Juta   =" +c);
+        System.out.println("Satu Miliar =" +d);
+    }
+    
+}
