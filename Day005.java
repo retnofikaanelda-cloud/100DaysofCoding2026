7d5ed52382e69aa5f1b0bfd176b443e304ed8fb9
Retno fika anelda <retnofikaanelda@gmail.com>
2026-09-06 06:16:40 +0800

    Create Day005.java

---
+++ Day005.java
@@ -0,0 +1,11 @@
+package Day005;
+public class Day005 {
+    public static void main(String[] args) {
+        double bb = 42.5;
+        float tb = 153.5f;
+      
+        System.out.println("Berat Badan = "+bb +" kg");
+        System.out.println("Tinggi Badan = "+tb+" cm");
+        
+    }
+}
