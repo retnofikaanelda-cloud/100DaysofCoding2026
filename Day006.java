4a77bdb348efc1cedf5816917023a484b15403e0
Retno fika anelda <retnofikaanelda@gmail.com>
2026-09-07 06:37:38 +0800

    Create Day006.java

---
+++ Day006.java
@@ -0,0 +1,15 @@
+package Day006;
+public class Day006 {
+    public static void main(String[]args){
+    char jk = 'P';
+    char at = '@';
+    
+    boolean mhs = true;
+    boolean k = false;
+    
+    System.out.println("Jenis Kelamin :"+ jk);
+    System.out.println("retnofikaanelda"+at+ "gmail.com");
+    System.out.println("Mahasiswa : "+mhs);
+    System.out.println("Kerja     : "+k);
+    }
+}
