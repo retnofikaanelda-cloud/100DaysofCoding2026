890db5a1d8af393df3c7ee857678d7e9c5272700
Retno fika anelda <retnofikaanelda@gmail.com>
2026-09-08 06:03:03 +0800

    Create Day007.java

---
+++ Day007.java
@@ -0,0 +1,9 @@
+package Day007;
+public class Day007 {
+    public static void main (String[]args){
+        
+        String nama ="Retno fika anelda";
+        System.out.println (nama);
+    }
+    
+}
