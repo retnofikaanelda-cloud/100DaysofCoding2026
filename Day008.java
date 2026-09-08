20cc0008bf626ad752492c4549fd77a8cf7e13ea
Retno fika anelda <retnofikaanelda@gmail.com>
2026-09-09 05:06:50 +0800

    Create Day008.java

---
+++ Day008.java
@@ -0,0 +1,17 @@
+package Day008;
+public class Day008 {
+    public static void main (String[]args){
+        String nama;
+        int umur;
+        
+        nama = "Retno fika anelda";
+        umur = 18;
+        
+        System.out.println ("Nama :"+ nama);
+        System.out.println ("Umur :"+ umur + " Tahun" );
+        
+        umur=19;
+        System.out.println ("update umur :" + umur + " Tahun");
+    }
+    
+}
