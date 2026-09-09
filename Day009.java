018230bd461fdcc19d0ffbe0f309b47650129e7e
Retno fika anelda <retnofikaanelda@gmail.com>
2026-09-10 00:08:18 +0800

    Create Day009.java

---
+++ Day009.java
@@ -0,0 +1,12 @@
+package Day009;
+public class Day009 {
+    public static void main (String[]args){
+        final String NAMA = "Retno fika anelda";
+        final int UMUR = 18;
+        final String KAMPUS = "UNSULBAR";
+
+        System.out.println("Nama   : " + NAMA);
+        System.out.println("Umur   : " + UMUR);
+        System.out.println("Kampus : " + KAMPUS);
+    }
+}
