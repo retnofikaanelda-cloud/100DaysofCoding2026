942e0546a28b3b976cb030796ec2b14833925e47
Retno fika anelda <retnofikaanelda@gmail.com>
2026-09-11 18:19:15 +0800

    Create Day010.java

---
+++ Day010.java
@@ -0,0 +1,38 @@
+package Day010;
+public class Day010 {
+    public static void main(String[] args) {
+
+        System.out.println("BYTE");
+        System.out.println("Minimal = " + Byte.MIN_VALUE);
+        System.out.println("Maksimal = " + Byte.MAX_VALUE);
+
+        System.out.println("\nSHORT");
+        System.out.println("Minimal = " + Short.MIN_VALUE);
+        System.out.println("Maksimal = " + Short.MAX_VALUE);
+
+        System.out.println("\nINT");
+        System.out.println("Minimal = " + Integer.MIN_VALUE);
+        System.out.println("Maksimal = " + Integer.MAX_VALUE);
+
+        System.out.println("\nLONG");
+        System.out.println("Minimal = " + Long.MIN_VALUE);
+        System.out.println("Maksimal = " + Long.MAX_VALUE);
+
+        System.out.println("\nFLOAT");
+        System.out.println("Minimal = " + -Float.MAX_VALUE);
+        System.out.println("Maksimal = " + Float.MAX_VALUE);
+
+        System.out.println("\nDOUBLE");
+        System.out.println("Minimal = " + -Double.MAX_VALUE);
+        System.out.println("Maksimal = " + Double.MAX_VALUE);
+
+        System.out.println("\nCHAR");
+        System.out.println("Minimal = " + (int) Character.MIN_VALUE);
+        System.out.println("Maksimal = " + (int) Character.MAX_VALUE);
+
+        System.out.println("\nBOOLEAN");
+        System.out.println("Minimal = false");
+        System.out.println("Maksimal = true");
+    }
+}
+    
