738c520f35a6e898325c10a1f99371a8600844d5
Retno fika anelda <retnofikaanelda@gmail.com>
2026-09-15 20:04:45 +0800

    Create Day014.java

---
+++ Day014.java
@@ -0,0 +1,13 @@
+package Day014;
+public class Day014 {
+    public static void main (String[]args){
+        int angka1 = 30;
+        int angka2 = 20;
+
+        int penjumlahan = angka1 + angka2;
+        int pengurangan = angka1 - angka2;
+
+        System.out.println("Penjumlahan = " + penjumlahan);
+        System.out.println("Pengurangan = " + pengurangan);
+    }
+}
