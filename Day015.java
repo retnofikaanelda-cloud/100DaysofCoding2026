44f36becafb73be98e1a92cf54d3334b0715217e
Retno fika anelda <retnofikaanelda@gmail.com>
2026-09-16 20:20:26 +0800

    Create Day015.java

---
+++ Day015.java
@@ -0,0 +1,14 @@
+package Day015;
+public class Day015 {
+    public static void main(String[]args){
+        int a = 15;
+        int b = 3;
+        
+        int perkalian = a * b;
+        int pembagian = a / b;
+        
+        System.out.println("Hasil Perkalian =" + perkalian);
+        System.out.println("Hasil Pembagian =" + pembagian);
+    }
+    
+}
