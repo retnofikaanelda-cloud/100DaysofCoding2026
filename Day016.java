c80e56ed317ad5a472cd7d71cbfc65ca9b39e907
Retno fika anelda <retnofikaanelda@gmail.com>
2026-09-17 19:34:20 +0800

    Create Day016.java

---
+++ Day016.java
@@ -0,0 +1,14 @@
+package Day016;
+public class Day016 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        int c = 3;
+        
+        int modulus1 = a % b;
+        int modulus2 = a % c;
+        
+        System.out.println("Hasil 1 :" + modulus1);
+        System.out.println("Hasil 2 :" + modulus2);
+    }
+}
