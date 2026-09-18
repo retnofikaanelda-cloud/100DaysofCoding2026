faef90adce33061d21c705f9b3f402c6eadb7d85
Retno fika anelda <retnofikaanelda@gmail.com>
2026-09-18 19:16:14 +0800

    Update Day017.java

--- Day017.java
+++ Day017.java
@@ -1,16 +1,18 @@
 package Day017;
 public class Day017 {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 2;
-        int c = 3;
-        
-        a = b;
-        b += c;
-        c -= a;
-        
-        System.out.println("Hasil a :" + a);
-        System.out.println("Hasil b :" + b);
-        System.out.println("Hasil c :" + c);
+        int a ;
+        a = 10;
+        System.out.println("Hasil a = 10 : " + a);
+        a += 5;
+        System.out.println("Hasil a += 5 : " + a);
+        a -= 5;
+        System.out.println("Hasil a -= 5 : "+ a);
+        a *= 5;
+        System.out.println("Hasil a *= 5 : "+ a);
+        a /= 5;
+        System.out.println("Hasil a /= 5 : "+ a);
+        a %= 5;
+        System.out.println("Hasil a %= 5 : "+ a); 
     }
 }
