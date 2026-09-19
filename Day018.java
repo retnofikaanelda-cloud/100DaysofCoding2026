36644f270874db185227998f3cd5f4416ba12737
Retno fika anelda <retnofikaanelda@gmail.com>
2026-09-19 21:48:05 +0800

    Create Day018.java

---
+++ Day018.java
@@ -0,0 +1,24 @@
+package Day018;
+public class Day018 {
+    public static void main(String[] args) {
+        byte a = 10;
+        short b = 20;
+        int c = 30;
+        long d = 40;
+        short hasil1 = a;
+        int hasil2 = b;
+        long hasil3 = c;
+        float hasil4 = d;
+        double hasil5 = c;
+        System.out.println("Nilai byte   = " + a);
+        System.out.println("Nilai short  = " + b);
+        System.out.println("Nilai int    = " + c);
+        System.out.println("Nilai long   = " + d);
+        System.out.println();
+        System.out.println("Byte ke Short   = " + hasil1);
+        System.out.println("Short ke Int    = " + hasil2);
+        System.out.println("Int ke Long     = " + hasil3);
+        System.out.println("Long ke Float   = " + hasil4);
+        System.out.println("Int ke Double   = " + hasil5);
+    }
+}
