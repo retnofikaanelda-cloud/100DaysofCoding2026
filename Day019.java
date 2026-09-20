f5c4f142deb0c48bf4f1ed6fa908852d03096e1e
Retno fika anelda <retnofikaanelda@gmail.com>
2026-09-20 21:11:01 +0800

    Create Day019.java

---
+++ Day019.java
@@ -0,0 +1,17 @@
+package Day019;
+public class Day019 {
+    public static void main(String[] args) {
+        
+        long a = 100;
+        int b = 50;
+        short c = 25;
+
+        int hasil1 = (int) a;
+        short hasil2 = (short) b;
+        byte hasil3 = (byte) c;
+
+        System.out.println("Hasil 1 : " + hasil1);
+        System.out.println("Hasil 2 : " + hasil2);
+        System.out.println("Hasil 3 : " + hasil3);
+    }
+}
