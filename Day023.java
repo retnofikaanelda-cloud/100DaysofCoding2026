68f436a9b4cc378b9a78d867d9221fb5a0c8f3e1
Retno fika anelda <retnofikaanelda@gmail.com>
2026-09-24 15:28:37 +0800

    Create Day023.java

---
+++ Day023.java
@@ -0,0 +1,11 @@
+package Day023;
+public class Day023{
+    public static void main(String[] args) {
+        int sisi = 5;
+        
+        int luas = sisi * sisi;
+        
+        System.out.println("Sisi         = " + sisi);
+        System.out.println("Luas persegi = " + luas);
+    }
+}
