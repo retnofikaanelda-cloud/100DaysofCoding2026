87f9686ab338259e6a63854b3cc26877257f5c0f
Retno fika anelda <retnofikaanelda@gmail.com>
2026-09-25 20:53:31 +0800

    Create Day024.java

---
+++ Day024.java
@@ -0,0 +1,14 @@
+package Day024;
+public class Day024 {
+    public static void main(String[] args) {
+        int panjang = 10;
+        int lebar = 5;
+
+        int luas = panjang * lebar;
+
+        System.out.println("Panjang = " + panjang);
+        System.out.println("Lebar = " + lebar);
+        System.out.println("Luas persegi panjang = " + luas);
+    
+ }
+}
