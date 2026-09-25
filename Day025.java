49cd387d34fd0717b596bcb144160f641ab1a645
Retno fika anelda <retnofikaanelda@gmail.com>
2026-09-26 05:56:10 +0800

    Create Day025.java

---
+++ Day025.java
@@ -0,0 +1,11 @@
+package Day025;
+public class Day025 {
+    public static void main(String[] args) {
+
+        double jariJari = 21;
+        double luas = (22.0/ 7.0)* jariJari * jariJari;
+
+        System.out.println("Jari-jari      = " + jariJari);
+        System.out.println("Luas Lingkaran = " + luas);
+    }
+}
